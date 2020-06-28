8116811d86c7c7d6de95a4e53ca09833b39493f8
Manu The Elite <55787181+Emmanuel-Murimi@users.noreply.github.com>
2020-06-28 12:54:08 +0300

    Delete formstrial.php

--- formstrial.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<form method="post" action="formdata.php">
-Name : <input type="text" name="fname"><br>
-Email : <input type="text" name="email"><br>
-<input type="Submit">
-</form>
-</body>
-</html>
