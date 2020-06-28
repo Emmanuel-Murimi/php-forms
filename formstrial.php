429cbba66578a6ad63ec24fc1162f860fa668fa1
Manu The Elite <55787181+Emmanuel-Murimi@users.noreply.github.com>
2020-06-28 12:52:09 +0300

    Add files via upload

---
+++ formstrial.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form method="post" action="formdata.php">
+Name : <input type="text" name="fname"><br>
+Email : <input type="text" name="email"><br>
+<input type="Submit">
+</form>
+</body>
+</html>
